6496d29be65151f2a4be6de8115fb25dad6fdb23
Ummadi Rama Rangeswara Reddy <ramarangeswara.reddy@sen-sei.in>
2023-08-20 10:49:23 +0530

    added the code after starting the server

--- src/main/java/com/example/firstproject/FirstprojectApplication.java
+++ src/main/java/com/example/firstproject/FirstprojectApplication.java
@@ -1,13 +1,25 @@
 package com.example.firstproject;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {"com.example.firstproject", "com.outside.service.impl"})
 public class FirstprojectApplication {
+
+	@Value("${server.port}")
+	private String port;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstprojectApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Server started in POST 8081 ... " + port);
+		};
+	}
 }
