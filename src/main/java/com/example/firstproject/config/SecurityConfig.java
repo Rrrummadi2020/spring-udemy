b968557cd5c8448063c05443db6b4752afb6f17b
Ummadi Rama Rangeswara Reddy <ramarangeswara.reddy@sen-sei.in>
2023-09-10 09:26:04 +0530

    custom table authentication

--- src/main/java/com/example/firstproject/config/SecurityConfig.java
+++ src/main/java/com/example/firstproject/config/SecurityConfig.java
@@ -40,6 +40,9 @@ public class SecurityConfig {
     
     @Bean
     public JdbcUserDetailsManager userDetailsManager(DataSource dataSource){
-        return new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager jdbcUserDetailsManager=  new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.setUsersByUsernameQuery("SELECT user_id, pw,active FROM members WHERE user_id=?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("SELECT user_id,role FROM roles WHERE user_id=?;");
+        return jdbcUserDetailsManager;//created two new tables members & roles custom tables
     }
 }
